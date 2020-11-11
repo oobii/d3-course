1b497130c025d998ca90baccb6757836a1e2d252
oobii <vvayfarer@yandex.com>
2020-11-11 17:12:09 -0500

    svg circle rect

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # d3-course
+
+npm init -y
+
+npm install eslint
+npx eslint --init
+// run periodically to check for style warrnings
+npx eslint .
+ or
+ npx eslint app.js
+
--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-d3.select("p")
-.style("color","red");
+// import { svg } from "./d3.v6";
+
+// // d3.select("p")
+// // .style("color","red");
+
+const canvas = d3.select('.canva');
+const svg = canvas.append('svg')
+    .attr('width', 500)
+    .attr('height', 250);
+
+svg.append('circle')
+    .attr('cx', 100)
+    .attr('cy', 100)
+    .attr('r', 50)
+    .attr('fill', 'blue');
+
+svg.append('rect')
+    .attr('x', 100)
+    .attr('y', 100)
+    .attr('width', 70)
+    .attr('height', 60)
+    .attr('fill', 'red');
