3d6cd6dc0164401fa3c9151c475905cfa6c15476
oobii <vvayfarer@yandex.com>
2020-11-11 23:21:21 -0500

    bars from array of objects

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const canvas = d3.select('.canva');
 const svg = canvas.append('svg')
-                    .attr('width', 500)
-                    .attr('height', 500);
+    .attr('width', 500)
+    .attr('height', 500);
 
-// svg.append('circle')
-//                     .attr('cx', 100)
-//                     .attr('cy', 100)
-//                     .attr('r', 50)
-//                     .attr('fill', 'blue');
 
 const rect = svg.selectAll('rect');
 
-rect.data([20, 50, 10, 75, 5])
+const myArrayOfObj = [
+  {width: 25, height: 23, fill: 'pink'},
+  {width: 25, height: 56, fill: 'orange'},
+  {width: 25, height: 15, fill: 'red'},
+  {width: 25, height: 77, fill: 'green'},
+  {width: 25, height: 20, fill: 'blue'},
+];
+rect.data(myArrayOfObj)
     .enter().append('rect')
-    .attr('fill', 'orange')
+    .attr('fill', d => d.fill)
     .attr('width', 25)
-    .attr('height', (d, i) => {return d;})
-    .attr('x', (d, i) => { return i*26; })
-    .attr('y', (d, i) => {return 150 - d;});
+    .attr('height', (d, i) => {
+      return d.height * 2;
+    })
+    .attr('x', (d, i) => {
+      return i * (d.width + 1);
+    })
+    .attr('y', (d, i) => {
+      return 200 - d.height * 2;
+    });
 
